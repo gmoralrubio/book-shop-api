7358b8fd223978aaabbd81ca77647736a1725324
guille <gmoralrubio@gmail.com>
2026-06-28 18:36:49 +0200

    chore: added path aliases

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,10 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src/__tests__'],
   testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '^@domain(.*)$': '<rootDir>/src/domain$1',
+    '^@infraestructure(.*)$': '<rootDir>/src/infraestructure$1',
+    '^@ui(.*)$': '<rootDir>/src/ui$1',
+    '^@__tests__(.*)$': '<rootDir>/src/__tests__$1',
+  },
 };
