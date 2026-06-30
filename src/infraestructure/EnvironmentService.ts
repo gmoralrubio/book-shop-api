73d1c5debc20cc5e200045de83e9fd5f5da11932
guille <gmoralrubio@gmail.com>
2026-06-30 11:35:54 +0200

    feat: added environment variables service

---
+++ src/infraestructure/EnvironmentService.ts
@@ -0,0 +1,43 @@
+import z, { ZodError } from 'zod';
+import dotenv from 'dotenv';
+
+// Validation schema
+const environmentVariablesValidator = z.object({
+  DB_URL: z.string(),
+  JWT_SECRET: z.string(),
+  NODE_ENV: z.enum(['local', 'staging', 'production', 'test']),
+  PORT: z.coerce.number(),
+});
+
+type EnvironmentVariables = z.infer<typeof environmentVariablesValidator>;
+
+class EnvironmentService {
+  private environmentVariables: EnvironmentVariables | null = null;
+
+  load() {
+    if (this.environmentVariables) return;
+
+    dotenv.config();
+
+    try {
+      this.environmentVariables = environmentVariablesValidator.parse(
+        process.env
+      );
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new Error(
+          'Error loading environment variables: ' + JSON.stringify(error)
+        );
+      }
+    }
+  }
+
+  get(): EnvironmentVariables {
+    if (!this.environmentVariables) {
+      throw new Error('Environment variables not loaded. Call .load() first');
+    }
+    return this.environmentVariables;
+  }
+}
+
+export const environmentService = new EnvironmentService();
