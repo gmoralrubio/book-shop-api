d1569e88dee9cf67254979499436d089a613ec89
guille <gmoralrubio@gmail.com>
2026-06-30 13:12:41 +0200

    feat: extracted user credentials validation

---
+++ src/ui/user/user-credentials-validation.ts
@@ -0,0 +1,17 @@
+import z from 'zod';
+
+export const userCredentialsValidationSchema = z.object({
+  email: z.email({
+    error: (iss) =>
+      iss.input === undefined
+        ? 'Email is required'
+        : 'Email does not comply with validation rules',
+  }),
+  password: z
+    .string({
+      error: 'Password is required',
+    })
+    .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,20}$/, {
+      error: 'Password does not comply with validation rules',
+    }),
+});
