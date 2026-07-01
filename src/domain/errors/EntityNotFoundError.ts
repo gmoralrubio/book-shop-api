6b2837266334978dbbc1f36d70364c3766bc3a74
guille <gmoralrubio@gmail.com>
2026-07-02 00:31:35 +0200

    fix: modified id type from string to number

--- src/domain/errors/EntityNotFoundError.ts
+++ src/domain/errors/EntityNotFoundError.ts
@@ -3,7 +3,7 @@ import { DomainError } from '@domain/errors/DomainError';
 export class EntityNotFoundError extends DomainError {
   readonly name = 'EntityNotFoundError';
 
-  constructor(entity: string, id: string) {
+  constructor(entity: string, id: number) {
     super(`Entity ${entity} not found with id ${id}`);
   }
 }
