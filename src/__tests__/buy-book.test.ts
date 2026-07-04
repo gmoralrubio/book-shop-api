909def1cf705d802979f926dd23e24f356f7d509
guille <gmoralrubio@gmail.com>
2026-07-04 12:50:01 +0200

    test: added buy book tests

---
+++ src/__tests__/buy-book.test.ts
@@ -0,0 +1,104 @@
+import { createBook } from '@__tests__/test-utils/book-test-utils';
+import { prisma } from '@__tests__/test-utils/prisma-client';
+import { loginUser, signupUser } from '@__tests__/test-utils/user-test-utils';
+import api from '../api';
+import { environmentService } from '@infraestructure/EnvironmentService';
+import request from 'supertest';
+
+beforeAll(() => {
+  environmentService.load();
+});
+
+beforeEach(async () => {
+  await prisma.book.deleteMany();
+  await prisma.user.deleteMany();
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
+});
+
+const testUserA = {
+  email: 'valid.email.a@test.com',
+  password: 'validPassword1A!',
+};
+const testUserB = {
+  email: 'valid.email.b@test.com',
+  password: 'validPassword1B!',
+};
+
+describe('POST /books/:id/buy', () => {
+  test('Given valid data, an authenticated user can buy another users book', async () => {
+    await signupUser(testUserA);
+    const tokenUserA = await loginUser(testUserA);
+    await signupUser(testUserB);
+    const tokenUserB = await loginUser(testUserB);
+
+    const newBook = await createBook(tokenUserA);
+
+    expect(newBook.status).toEqual(201);
+
+    const id = newBook.body.id;
+    const response = await request(api)
+      .post(`/books/${id}/buy`)
+      .auth(tokenUserB, { type: 'bearer' });
+
+    expect(response.status).toEqual(200);
+    expect(response.body.status).toEqual('SOLD');
+  });
+
+  test('An error with status code 404 is returned if the book doesnt exist', async () => {
+    await signupUser(testUserA);
+    const token = await loginUser(testUserA);
+    const invalidId = '999';
+
+    const response = await request(api)
+      .post(`/books/${invalidId}/buy`)
+      .auth(token, { type: 'bearer' });
+
+    expect(response.status).toEqual(404);
+    expect(response.body.error).toEqual(
+      `Entity book not found with id ${invalidId}`
+    );
+  });
+
+  test('An error with status code 409 is returned if the book status is "SOLD"', async () => {
+    await signupUser(testUserA);
+    const tokenUserA = await loginUser(testUserA);
+    await signupUser(testUserB);
+    const tokenUserB = await loginUser(testUserB);
+
+    const newBook = await createBook(tokenUserA);
+
+    expect(newBook.status).toEqual(201);
+
+    const id = newBook.body.id;
+    await request(api)
+      .post(`/books/${id}/buy`)
+      .auth(tokenUserB, { type: 'bearer' });
+
+    const response = await request(api)
+      .post(`/books/${id}/buy`)
+      .auth(tokenUserB, { type: 'bearer' });
+
+    expect(response.status).toEqual(409);
+    expect(response.body.error).toEqual(
+      'Users cannot buy a book with SOLD status'
+    );
+  });
+
+  test('An error with status code 409 is returned if the user is the book owner', async () => {
+    await signupUser(testUserA);
+    const token = await loginUser(testUserA);
+    const newBook = await createBook(token);
+
+    const id = newBook.body.id;
+
+    const response = await request(api)
+      .post(`/books/${id}/buy`)
+      .auth(token, { type: 'bearer' });
+
+    expect(response.status).toEqual(409);
+    expect(response.body.error).toEqual('Users cannot buy their own books');
+  });
+});
