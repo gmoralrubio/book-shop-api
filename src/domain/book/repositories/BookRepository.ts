6c8d2272d4e55d67b076a34d68684e573f623bbc
guille <gmoralrubio@gmail.com>
2026-07-06 17:01:06 +0200

    refactor: unified find book use case

--- src/domain/book/repositories/BookRepository.ts
+++ src/domain/book/repositories/BookRepository.ts
@@ -1,14 +1,14 @@
 import { Book, BookStatus } from '@domain/book/Book';
 import { FindBooksResponse } from '@domain/book/types/FindBooksResponse';
 import { CreateBookUseCaseInput } from '@domain/book/use-cases/create-book';
-import { FindBooksMeUseCaseInput } from '@domain/book/use-cases/find-books';
+import { FindBooksUseCaseInput } from '@domain/book/use-cases/find-books';
 import { UpdateBookUseCaseInput } from '@domain/book/use-cases/update-book';
 
 export interface BookRepository {
   create(params: CreateBookUseCaseInput): Promise<Book>;
   update(params: UpdateBookUseCaseInput): Promise<Book>;
   delete(id: number): Promise<void>;
-  findMany(criteria: FindBooksMeUseCaseInput): Promise<FindBooksResponse>;
+  findMany(criteria: FindBooksUseCaseInput): Promise<FindBooksResponse>;
   findById(id: number): Promise<Book | null>;
   toggleStatusTo(status: BookStatus, id: number): Promise<void>;
   setSoldAt(date: Date, id: number): Promise<void>;
--- src/domain/book/use-cases/find-books.ts
+++ src/domain/book/use-cases/find-books.ts
@@ -1,21 +1,24 @@
 import { FindBooksResponse } from '@domain/book/types/FindBooksResponse';
 import { BookRepository } from '@domain/book/repositories/BookRepository';
 import { Pagination } from '@domain/shared/Pagination';
+import { BookStatus } from '@domain/book/Book';
 
-interface BookMeFilterQuery {
+interface BookFilterQuery {
   userId?: number;
+  search?: string;
+  status?: BookStatus;
 }
 
-export type FindBooksMeUseCaseInput = Pagination & BookMeFilterQuery;
+export type FindBooksUseCaseInput = Pagination & BookFilterQuery;
 
-export class FindBooksMeUseCase {
+export class FindBooksUseCase {
   readonly bookRepository: BookRepository;
 
   constructor(bookRepository: BookRepository) {
     this.bookRepository = bookRepository;
   }
 
-  async execute(criteria: FindBooksMeUseCaseInput): Promise<FindBooksResponse> {
+  async execute(criteria: FindBooksUseCaseInput): Promise<FindBooksResponse> {
     const { books, total } = await this.bookRepository.findMany(criteria);
     return {
       books,
