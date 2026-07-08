3382f53d527843891f793ac9e06704d07ae63208
guille <gmoralrubio@gmail.com>
2026-07-08 13:46:13 +0200

    fix: moved seed methods to utils folder inside src/ to make them accesible to the test files

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,64 +1,12 @@
-import { PrismaClient } from '@prisma/client';
-import { faker } from '@faker-js/faker';
-import bcrypt from 'bcrypt';
+import { seedDB } from '../src/utils/seed-utils';
+import prismaClient from '../src/infraestructure/shared/prisma-client';
 
-const prisma = new PrismaClient();
-
-async function main() {
-  await seedUsers();
-  await seedBooks(10);
-}
-
-async function seedUsers() {
-  const salt = await bcrypt.genSalt(10);
-
-  await prisma.user.upsert({
-    where: { email: 'john.doe@email.com' },
-    update: {},
-    create: {
-      email: 'john.doe@email.com',
-      password: await bcrypt.hash('validPassword123!', salt),
-    },
-  });
-  await prisma.user.upsert({
-    where: { email: 'admin@email.com' },
-    update: {},
-    create: {
-      email: 'admin@email.com',
-      password: await bcrypt.hash('validPassword123!', salt),
-    },
-  });
-  console.log('Users seeded');
-}
-
-async function seedBooks(numBooks: number) {
-  for (let i = 1; i <= numBooks; i++) {
-    const status = faker.helpers.arrayElement(['PUBLISHED', 'SOLD']);
-    const soldAt = status === 'PUBLISHED' ? null : new Date();
-    await prisma.book.upsert({
-      where: { id: i },
-      update: {},
-      create: {
-        id: i,
-        title: faker.book.title(),
-        description: faker.lorem.words(10),
-        price: faker.number.float({ min: 1, max: 50, fractionDigits: 2 }),
-        author: faker.book.author(),
-        ownerId: faker.helpers.arrayElement([1, 2]),
-        status,
-        soldAt,
-      },
-    });
-  }
-  console.log('Books seeded');
-}
-
-main()
+seedDB()
   .then(async () => {
-    await prisma.$disconnect();
+    await prismaClient.$disconnect();
   })
   .catch(async (e) => {
     console.error(e);
-    await prisma.$disconnect();
+    await prismaClient.$disconnect();
     process.exit(1);
   });
