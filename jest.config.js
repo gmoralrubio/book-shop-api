dbe347e3b8cb2f37a65e6b738b92c09cff861045
guille <gmoralrubio@gmail.com>
2026-07-08 13:49:54 +0200

    feat: added support for jest to read faker dependency

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,15 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src/__tests__'],
   testMatch: ['**/*.test.ts'],
+  // Compila los .js de faker
+  transform: {
+    '^.+\\.[tj]sx?$': ['ts-jest'],
+  },
+  transformIgnorePatterns: ['/node_modules/(?!@faker-js/)'],
   moduleNameMapper: {
+    '^@__tests__(.*)$': '<rootDir>/src/__tests__$1',
     '^@domain(.*)$': '<rootDir>/src/domain$1',
     '^@infraestructure(.*)$': '<rootDir>/src/infraestructure$1',
     '^@ui(.*)$': '<rootDir>/src/ui$1',
-    '^@__tests__(.*)$': '<rootDir>/src/__tests__$1',
   },
 };
