a4990c579f4799834abf42ddec1bf37efe755d4d
guille <gmoralrubio@gmail.com>
2026-07-09 19:29:07 +0200

    feat: added email service implementation with nodemailer

---
+++ src/infraestructure/shared/NodemailerEmailService.ts
@@ -0,0 +1,25 @@
+import nodemailer from 'nodemailer';
+import { EmailService, SendEmailParams } from '@domain/shared/EmailService';
+import { environmentService } from '@infraestructure/EnvironmentService';
+
+export class NodemailerEmailService implements EmailService {
+  private readonly transporter: nodemailer.Transporter;
+
+  constructor() {
+    const { MAILDEV_HOST, MAILDEV_PORT } = environmentService.get();
+
+    this.transporter = nodemailer.createTransport({
+      host: MAILDEV_HOST,
+      port: MAILDEV_PORT,
+      secure: false,
+      ignoreTLS: true,
+    });
+  }
+  async send(params: SendEmailParams): Promise<void> {
+    await this.transporter.sendMail({
+      from: 'BookShop <noreply@bookshop.com>',
+      to: params.email,
+      text: params.message,
+    });
+  }
+}
