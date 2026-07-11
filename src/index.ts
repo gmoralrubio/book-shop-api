cffc9179725ed85ff6d0a7ef3cecef6b33611b36
guille <gmoralrubio@gmail.com>
2026-07-11 17:21:21 +0200

    feat: added price review cron and worker

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { environmentService } from '@infraestructure/shared/services/EnvironmentService';
 import api from './api';
-
+import { environmentService } from '@infraestructure/shared/services/EnvironmentService';
 import { SoldBookEmailWorker } from '@infraestructure/shared/workers/SoldBookEmailWorker';
 import { NodemailerEmailService } from '@infraestructure/shared/services/NodemailerEmailService';
 import { PrismaUserRepository } from '@infraestructure/user/repositories/PrismaUserRepository';
+import { PrismaBookRepository } from '@infraestructure/book/repositories/PrismaBookRepository';
+import { ReviewPriceEmailWorker } from '@infraestructure/shared/workers/ReviewPriceEmailWorker';
+import { BullQueueService } from '@infraestructure/shared/services/BullQueueService';
 
 environmentService.load();
 
@@ -15,5 +17,10 @@ api.listen(PORT, () => {
 
 const emailService = new NodemailerEmailService();
 const userRepository = new PrismaUserRepository();
+const bookRepository = new PrismaBookRepository();
 
 new SoldBookEmailWorker(emailService, userRepository);
+new ReviewPriceEmailWorker(emailService, userRepository, bookRepository);
+
+const bullQueueService = new BullQueueService();
+bullQueueService.priceReviewCron();
