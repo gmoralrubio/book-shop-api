28cc36db729e2bdd421f70bf5e474608b55a79d7
guille <gmoralrubio@gmail.com>
2026-07-11 12:22:14 +0200

    feat: added default job options to sold book email queue

---
+++ src/infraestructure/shared/services/BullQueueService.ts
@@ -0,0 +1,34 @@
+import { QueueService, SoldBookEmailParams } from '@domain/shared/QueueService';
+import { environmentService } from '@infraestructure/shared/services/EnvironmentService';
+import { Queue } from 'bullmq';
+
+export class BullQueueService implements QueueService {
+  private readonly soldBookEmailQueue: Queue;
+  constructor() {
+    this.soldBookEmailQueue = new Queue('sold-book-email', {
+      connection: BullQueueService.getRedisConnection(),
+      defaultJobOptions: {
+        attempts: 3,
+        backoff: {
+          type: 'exponential',
+          delay: 1000,
+        },
+        removeOnComplete: 1000,
+        removeOnFail: 5000,
+      },
+    });
+  }
+
+  static getRedisConnection() {
+    const { REDIS_URL } = environmentService.get();
+    const redisUrl = new URL(REDIS_URL);
+    return {
+      host: redisUrl.hostname,
+      port: Number(redisUrl.port),
+    };
+  }
+
+  async sendSoldBookEmail(params: SoldBookEmailParams): Promise<void> {
+    await this.soldBookEmailQueue.add('sold-book-email-job', params);
+  }
+}
