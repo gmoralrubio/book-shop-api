5f27dc469c310b54c6d6c2552f31e138261f7c65
guille <gmoralrubio@gmail.com>
2026-07-11 12:21:04 +0200

    feat: added sold book email worker class

---
+++ src/infraestructure/shared/workers/SoldBookEmailWorker.ts
@@ -0,0 +1,31 @@
+import { Job } from 'bullmq';
+import { SoldBookEmailParams } from '@domain/shared/QueueService';
+import { BullWorker } from '@infraestructure/shared/workers/BullWorker';
+import { EmailService } from '@domain/shared/EmailService';
+import { UserRepository } from '@domain/user/repositories/UserRepository';
+
+export class SoldBookEmailWorker extends BullWorker<SoldBookEmailParams> {
+  private readonly emailService: EmailService;
+  private readonly userRepository: UserRepository;
+
+  constructor(emailService: EmailService, userRepository: UserRepository) {
+    super('sold-book-email');
+    this.emailService = emailService;
+    this.userRepository = userRepository;
+  }
+
+  processJob = async (job: Job<SoldBookEmailParams>) => {
+    const user = await this.userRepository.findById(job.data.ownerId);
+
+    if (!user) {
+      throw new Error(
+        `User not found for sold book email: ownerId=${job.data.ownerId}`
+      );
+    }
+
+    await this.emailService.send({
+      email: user.email,
+      message: `The book ${job.data.title} has been sold`,
+    });
+  };
+}
