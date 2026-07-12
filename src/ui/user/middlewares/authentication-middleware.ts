e06c39fe4e98127c88e38d7e33ee505c29f3da10
guille <gmoralrubio@gmail.com>
2026-07-12 11:47:14 +0200

    fix: added try/catch to capture the errors

--- src/ui/user/middlewares/authentication-middleware.ts
+++ src/ui/user/middlewares/authentication-middleware.ts
@@ -1,28 +1,32 @@
 import { Request, Response, NextFunction } from 'express';
 import { UnauthorizedError } from '@domain/errors/UnauthorizedError';
-import { SecurityServiceImplementation } from '@infraestructure/user/services/SecurityServiceImplementation';
+import { SecurityServiceImplementation } from '@infrastructure/user/services/SecurityServiceImplementation';
 
 export const authenticationMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.headers.authorization;
+  try {
+    const token = req.headers.authorization;
 
-  if (!token) {
-    throw new UnauthorizedError('Token not in request');
-  }
+    if (!token) {
+      throw new UnauthorizedError('Token not in request');
+    }
 
-  const sanitizedToken = token.replace('Bearer ', '');
+    const sanitizedToken = token.replace('Bearer ', '');
 
-  const securityService = new SecurityServiceImplementation();
-  const decodedToken = securityService.verifyJWT(sanitizedToken);
+    const securityService = new SecurityServiceImplementation();
+    const decodedToken = securityService.verifyJWT(sanitizedToken);
 
-  req.userId = decodedToken?.userId;
+    req.userId = decodedToken?.userId;
 
-  if (decodedToken) {
-    next();
-  } else {
-    throw new UnauthorizedError('Token not valid');
+    if (decodedToken) {
+      next();
+    } else {
+      throw new UnauthorizedError('Token not valid');
+    }
+  } catch (error) {
+    next(error);
   }
 };
